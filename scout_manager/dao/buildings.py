02560dcf525b5403631440e5204efc91d21cb308
Steve Friedman <steve@commonmedia.com>
2017-02-14 14:55:47 -0500

    Smith: add Smith campus building codes to scout-manager, only show in admin, don't save to database

--- scout_manager/dao/buildings.py
+++ scout_manager/dao/buildings.py
@@ -1,389 +1,476 @@
 BUILDING_LIST = {
     "10 Prospect Street": {
-        'campus': 'smith'
-    },
-    "12 Bedford Terrace": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM108',
     },
     "126 West Street": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM312',
     },
     "138 Elm Street": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM101',
+    },
+    "146 Elm Street": {
+        'campus': 'smith',
+        'code': 'FM135',
     },
     "150 Elm Street": {
-        'campus': 'smith'
-    },
-    "26 Bedford Terrace": {
-        'campus': 'smith'
-    },
-    "27 Belmont Avenue ": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM470',
     },
     "30 Belmont Avenue": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM300',
     },
-    "32 Bedford Terrace": {
-        'campus': 'smith'
-    },
-    "42 Green Street": {
-        'campus': 'smith'
+    "42-44 Green Street": {
+        'campus': 'smith',
+        'code': 'FM562',
     },
     "51 College Lane": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM523',
     },
-    "52 Green Street": {
-        'campus': 'smith'
-    },
-    "54 Green Street": {
-        'campus': 'smith'
+    "52-54 Green Street": {
+        'campus': 'smith',
+        'code': 'FM535',
     },
     "76 Elm Street": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM107',
     },
     "8 College Lane": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM113',
     },
     "Admission Building": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM132',
     },
     "Ainsworth Gymnasium": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM130',
     },
     "Albright House": {
-        'campus': 'smith'
-    },
-    "Alumnae Gym": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM400',
     },
     "Alumnae House": {
-        'campus': 'smith'
-    },
-    "Athletic Fields": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM201',
     },
     "Baldwin House": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM401',
     },
     "Bass Hall": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM126',
     },
     "Bechtel Environmental Classroom": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM156',
     },
     "Boat House": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM102',
     },
     "Burton Hall": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM104',
+    },
+    "Burton Lawn": {
+        'campus': 'smith',
+        'code': 'FMZ09',
     },
     "Campus Center": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM222',
+    },
+    "Campus School": {
+        'campus': 'smith',
+        'code': 'FM311',
     },
     "Capen Annex": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM203',
     },
-    "Capen Boiler ": {
-        'campus': 'smith'
+    "Capen Boiler": {
+        'campus': 'smith',
+        'code': 'FM109',
     },
     "Capen Garage": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM302',
     },
     "Capen House": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM404',
+    },
+    "Center for Early Childhood Education": {
+        'campus': 'smith',
+        'code': 'FM215',
     },
     "Chapin House": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM406',
+    },
+    "Chapin Lawn": {
+        'campus': 'smith',
+        'code': 'FMZ08',
     },
     "Chase House": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM407',
     },
     "Chiller Plant": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM301',
+    },
+    "Civil Zone 1": {
+        'campus': 'smith',
+        'code': 'FMZ01',
+    },
+    "Civil Zone 2": {
+        'campus': 'smith',
+        'code': 'FMZ02',
+    },
+    "Civil Zone 3": {
+        'campus': 'smith',
+        'code': 'FMZ03',
+    },
+    "Civil Zone 4": {
+        'campus': 'smith',
+        'code': 'FMZ04',
+    },
+    "Civil Zone 5": {
+        'campus': 'smith',
+        'code': 'FMZ05',
+    },
+    "Civil Zone 6": {
+        'campus': 'smith',
+        'code': 'FMZ06',
     },
     "Clark Hall": {
-        'campus': 'smith'
-    },
-    "Clark Science Center": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM305',
     },
     "College Hall": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM304',
+    },
+    "College Lane": {
+        'campus': 'smith',
+        'code': 'FMZ07',
     },
     "Comstock House": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM410',
+    },
+    "Conference Center": {
+        'campus': 'smith',
+        'code': 'FM207',
     },
     "Conway House": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM405',
     },
     "Crew House": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM124',
     },
     "Cushing House": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM412',
     },
     "Cutter House": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM413',
     },
     "Davis Center": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM205',
     },
     "Dawes House": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM403',
     },
     "Dewey House": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM416',
     },
-    "Drew Hall ": {
-        'campus': 'smith'
+    "Drew Hall": {
+        'campus': 'smith',
+        'code': 'FM309',
     },
     "Duckett House": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM419',
     },
     "Emerson House": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM421',
     },
     "Equestrian Center": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM127',
     },
     "Facilities Management Garage": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM306',
     },
     "Field House": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM111',
     },
     "Fine Arts Center": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM112',
     },
     "Ford Hall": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM138',
     },
-    "Fort Hill Garage": {
-        'campus': 'smith'
-    },
-    "Fort Hill-Lyman Estate House": {
-        'campus': 'smith'
-    },
-    "Fort Hill, CECE": {
-        'campus': 'smith'
-    },
-    "Franklin King Garage": {
-        'campus': 'smith'
-    },
-    "Friedman Complex (A,B,C,D)": {
-        'campus': 'smith'
+    "Friedman Complex": {
+        'campus': 'smith',
+        'code': 'FM474',
     },
     "Gables": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM139',
     },
     "Gardiner House": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM423',
     },
     "Garrison Hall": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM313',
     },
     "Gill Hall": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM220',
     },
     "Gillett House": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM424',
     },
     "Graduate House": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM510',
     },
     "Green Street Classroom Annex": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM149',
     },
     "Grounds Garage": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM310',
     },
     "Hatfield Hall": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM116',
     },
     "Haven House": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM427',
     },
     "Helen Hills Hills Chapel": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM117',
     },
     "Hopkins House": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM428',
     },
     "Hubbard House": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM433',
     },
     "Indoor Track & Tennis Facility": {
-        'campus': 'smith'
-    },
-    "Japanese Tea Hut": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM118',
     },
     "John M. Greene Hall": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM114',
     },
     "Jordan House": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM435',
     },
     "King House": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM436',
     },
     "Lamont House": {
-        'campus': 'smith'
-    },
-    "Laundry - Campus School": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM438',
     },
     "Lawrence House": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM439',
     },
     "Lilly Hall": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM120',
     },
-    "Lyman Plant House": {
-        'campus': 'smith'
-    },
-    "Mason Hall": {
-        'campus': 'smith'
+    "Lyman Plant House & Conservatory": {
+        'campus': 'smith',
+        'code': 'FM122',
     },
     "McConnell Hall": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM121',
     },
     "Mendenhall CPA": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM103',
     },
     "Morgan Hall": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM123',
     },
     "Morris House": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM441',
     },
     "Morrow House": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM443',
     },
-    "Morton Building -Tennis Courts": {
-        'campus': 'smith'
-    },
-    "Neilson Library": {
-        'campus': 'smith'
+    "Neilson Lawn": {
+        'campus': 'smith',
+        'code': 'FMZ10',
     },
     "Northrop House": {
-        'campus': 'smith'
-    },
-    "Outdoor Track/Tennis Facilities": {
-        'campus': 'smith'
-    },
-    "Paradise Pond": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM445',
     },
     "Park Annex": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM447',
     },
     "Park House": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM446',
     },
     "Parking Garage": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM213',
     },
     "Parsons Annex": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM450',
     },
     "Parsons House": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM449',
     },
     "Pierce Hall": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM125',
     },
     "Power Plant": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM303',
     },
     "President's House": {
-        'campus': 'smith'
-    },
-    "Research Greenhouse": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM212',
     },
     "Sabin-Reed Hall": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM128',
     },
     "Sage Hall": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM129',
     },
     "Scales House": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM452',
     },
-    "Scott Gym": {
-        'campus': 'smith'
+    "Schacht Center": {
+        'campus': 'smith',
+        'code': 'FM216',
+    },
+    "Scott Gymnasium": {
+        'campus': 'smith',
+        'code': 'FM131',
     },
     "Seelye Hall": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM133',
     },
     "Sessions Annex": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM455',
     },
     "Sessions House": {
-        'campus': 'smith'
-    },
-    "Smith College Confererence Center": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM453',
     },
     "Stoddard Annex": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM144',
     },
     "Stoddard Hall": {
-        'campus': 'smith'
-    },
-    "Sunnyside": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM134',
     },
     "Talbot House": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM456',
     },
     "Tenney House": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM459',
     },
     "Tennis Hut": {
-        'campus': 'smith'
-    },
-    "The Nancy and Henry Schacht Center for Health and Wellness": {
-        'campus': 'smith'
-    },
-    "Tilly Hall": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM209',
     },
     "Tyler Annex": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM106',
     },
     "Tyler House": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM457',
     },
     "Unity House": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM225',
     },
     "Washburn House": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM460',
     },
     "Wesley House": {
-        'campus': 'smith'
-    },
-    "Whately Observatory": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM462',
     },
     "Wilder House": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM464',
     },
     "Wilson House": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM466',
     },
     "Wright Hall": {
-        'campus': 'smith'
+        'campus': 'smith',
+        'code': 'FM137',
     },
     "Ziskind House": {
-        'campus': 'smith'
-    },
+        'campus': 'smith',
+        'code': 'FM468',
+    }
 }
 
 def get_building_list():
-    return flatten_building_list(BUILDING_LIST)
+    return BUILDING_LIST
 
 
 def get_building_list_by_campus(campus):
@@ -391,11 +478,4 @@ def get_building_list_by_campus(campus):
     for building in BUILDING_LIST:
         if BUILDING_LIST[building]['campus'] == campus:
             buildings[building] = BUILDING_LIST[building]
-    return flatten_building_list(buildings)
-
-
-def flatten_building_list(buildings):
-    flat_list = {}
-    for building in buildings:
-        flat_list[building] = buildings[building]['campus']
-    return flat_list
+    return buildings
